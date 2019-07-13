7fd7dfb5ad5ce3e9f5e4abe87c22e49fba99e4e9
Kristina <gudaityte.kristina@gmail.com>
2019-07-13 15:40:04 +0200

    rest controller - searching by keyword - added

--- src/main/java/en/kristina/cookingbookapp/controller/RecipeRestController.java
+++ src/main/java/en/kristina/cookingbookapp/controller/RecipeRestController.java
@@ -1,4 +1,29 @@
 package en.kristina.cookingbookapp.controller;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import en.kristina.cookingbookapp.searcher.service.SearchRecipeService;
+
+import java.io.IOException;
+import java.util.*;
+import java.util.stream.Collectors;
+
+
+@RequiredArgsConstructor
+@Slf4j
+@RestController
+@RequestMapping("/api")
 public class RecipeRestController {
+
+	private final SearchRecipeService searchRecipeService;
+
+	@GetMapping("/recipe/{product}")
+	public List<String> allRecipesByproduct(@PathVariable String product) throws IOException {
+		Map<String, String> allRecipesByKeyword = searchRecipeService.findAllRecipesByKeyword(product);
+		return new ArrayList<>(allRecipesByKeyword.keySet());
+	}
 }
